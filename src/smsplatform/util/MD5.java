59f2a5f3890f0bc19d067cd257ffaf6659337f11
xitoujic <1033761115@qq.com>
2013-11-25 17:25:22 +0800

    Revert "second change"
    This reverts commit 967de3768d516790f0dc1f5e2cee6ecb73d27b47.

--- src/smsplatform/util/MD5.java
+++ src/smsplatform/util/MD5.java
@@ -1,11 +1,7 @@
 package smsplatform.util;
 
 import java.security.MessageDigest;
-/**
- * 
- * @author jiangnanEdu
- *
- */
+
 public class MD5 {
 	private final static String[] hexDigits = { "0", "1", "2", "3", "4", "5",
 			"6", "7", "8", "9", "a", "b", "c", "d", "e", "f" };
